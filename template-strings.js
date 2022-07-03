b6f61d2bfd6e5d964583849ef5028fbbdc3bdc78
unknown <romaoedu@hotmail.com>
2022-07-03 17:16:37 -0300

    minhas atualizações

---
+++ template-strings.js
@@ -0,0 +1,9 @@
+const nome = 'ju';
+const idade = 2022-1981;
+const cidadeDeNascimento = 'São Paulo';
+//const apresentacao = 'meu nome é ' + nome + ', minha idade é ' + idade + ' e moro na cidade de ' + cidadeDeNascimento;
+
+const apresentacao = `Meu nome é ${nome}, tenho ${idade} e nasci na cidade de ${cidadeDeNascimento}.`;
+
+console.log(apresentacao);
+
---
+++ ternario.js
@@ -0,0 +1,4 @@
+const idadeMinima = 18;
+const idadeCliente = 11;
+
+console.log(idadeCliente >= idadeMinima ? 'cerveja' : 'suco');
